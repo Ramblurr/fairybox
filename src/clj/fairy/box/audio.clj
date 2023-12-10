86260b29fb364245d22b8c8f8bc3a6341fc9fd86
Casey Link <unnamedrambler@gmail.com>
2023-12-10 19:10:17 +0100

    wire up audio player to bus

--- src/clj/fairy/box/audio.clj
+++ src/clj/fairy/box/audio.clj
@@ -1,15 +1,43 @@
 (ns fairy.box.audio
   (:require
+   [clojure.core.async :as async]
    [fairy.box.audio.interop :as interop]
    [clojure.tools.logging :as log]
    [integrant.core :as ig]))
 
-(defn init-audio [opts]
-  (log/info "\n-=[starting audio]=-")
-  {:player (interop/init-player)})
+(defn handler [player publisher {:keys [topic value] :as event}]
+  (println "player got: " value)
+  (when (= :bar (:foo value))
+    (async/put! publisher {:topic :player :value {:foo :baz}})))
 
-(defn halt-player! [{:keys [player]}]
-  (interop/release-player! player))
+(defn audio-loop [exit-ch subscriber {:keys [publication publisher] :as bus} init-state]
+  (log/info "\n-=[starting audio]=-")
+  (async/go-loop [player init-state]
+    (async/alt!
+      exit-ch ([_]
+               (log/info "\n-=[goodbye audio]=-")
+               (interop/release-player! player)
+               (async/unsub publication :player subscriber)
+               (async/close! exit-ch)
+               (async/close! subscriber)
+               nil)
+      subscriber ([ev]
+                  (handler player publisher ev)
+                  (recur player)))))
+
+(defn init-audio [{:keys [bus]}]
+  (let [subscriber (async/chan)
+        exit-ch (async/chan)
+        sub (async/sub (:publication bus) :player subscriber)
+        init-state (interop/init-player)]
+    (audio-loop exit-ch subscriber bus init-state)
+    {:subscriber subscriber
+     :sub sub
+     :exit-ch exit-ch
+     :state init-state}))
+
+(defn halt-player! [{:keys [exit-ch]}]
+  (async/put! exit-ch true))
 
 (defmethod ig/init-key ::player [_ opts]
   (init-audio opts))
