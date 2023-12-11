73395451415eb1bc14b16d0efacd4191e415b11f
Casey Link <unnamedrambler@gmail.com>
2023-12-11 12:57:54 +0100

    support LED and PWM LEDs

--- src/clj/fairy/box/hardware/led.clj
+++ src/clj/fairy/box/hardware/led.clj
@@ -2,13 +2,29 @@
   (:import
    [com.diozero.util Diozero]
    [com.diozero.util Diozero]
-   [com.diozero.devices LED]))
+   [com.diozero.devices LED PwmLed]))
 
-(defn init-led! [{:keys [gpio name]}]
-  (let [led (LED. gpio)]
+(defn init-led-led! [{:keys [gpio name active-high?]
+                      :or {active-high? true}}]
+  (let [led (LED. gpio
+                  active-high?
+                  false)]
     (Diozero/registerForShutdown (into-array LED [led]))
     (.on led)
     {:led led :gpio gpio :name name}))
+
+(defn init-pwm-led! [{:keys [gpio name]}]
+  (let [led (PwmLed. gpio 0.0)]
+    (Diozero/registerForShutdown (into-array PwmLed [led]))
+    (.on led)
+    (.setValue 1.0)
+    {:led led :gpio gpio :name name}))
+
+(defn init-led! [{:keys [led-type] :as opts}]
+  ;; (prn "init LED" name)
+  (case led-type
+    :led (init-led-led! opts)
+    :pwm (init-pwm-led! opts)))
 
 (defn init-leds! [{:keys [leds]}]
   {:leds
