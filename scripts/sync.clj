3fb647e53be4e26a413ebf08c4758ae34051032a
Casey Link <unnamedrambler@gmail.com>
2025-07-27 08:53:26 +0200

    DRY sync script

--- scripts/sync.clj
+++ scripts/sync.clj
@@ -1,9 +1,22 @@
 (ns sync
   (:require [babashka.tasks :refer [shell]]))
 
+#_(def host "fairybox-wifi")
+(def host "fairybox")
+
+(def excludes ["archive" ".git" "target" ".lsp" ".clj-kondo" ".cpcache"])
+(def exclude-args (mapcat (fn [x] ["--exclude" x]) excludes))
+
 (defn ^:export do-sync! [_]
-  (shell "rsync -e  \"ssh -o 'ControlPath=/dev/shm/control:%h:%p:%r'\" -avr --exclude archive --exclude .git --exclude target --exclude src --exclude .lsp --exclude .clj-kondo --exclude .cpcache ../fairybox/ fairybox-wifi:/var/lib/fairybox/fairybox")
-  (shell "rsync -e  \"ssh -o 'ControlPath=/dev/shm/control:%h:%p:%r'\" --delete -avr --exclude .git --exclude .lsp --exclude .clj-kondo --exclude .cpcache src/ fairybox-wifi:/var/lib/fairybox/fairybox/src"))
+  (shell (-> ["rsync" "-e" "ssh -o 'ControlPath=/dev/shm/control:%h:%p:%r'" "-avr" "--exclude" "src"]
+             (into exclude-args)
+             (conj "../fairybox/"
+                   (str host ":/var/lib/fairybox/fairybox"))))
+
+  (shell (-> ["rsync" "-e" "ssh -o 'ControlPath=/dev/shm/control:%h:%p:%r'" "--delete" "-avr"]
+             (into exclude-args)
+             (conj "src/"
+                   (str host ":/var/lib/fairybox/fairybox/src")))))
 
 (defn ^:export on-watch-event! [path]
   (do-sync! nil))
