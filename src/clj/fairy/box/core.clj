e379308dafa183d9e56de83a35179ca15c111f00
Casey Link <unnamedrambler@gmail.com>
2025-08-05 18:15:17 +0200

    Prepare core and -main for hifi

--- src/clj/fairy/box/core.clj
+++ src/clj/fairy/box/core.clj
@@ -1,27 +1,14 @@
 ;; Copyright © 2025 Casey Link <casey@outskirtslabs.com>
 ;; SPDX-License-Identifier: EUPL-1.2
 (ns fairy.box.core
+  (:require
+   [clojure.tools.logging :as log]
+   [fairy.box.system :as system]
+   [hifi.system :as hifi]
+   [hifi.util.shutdown :as shutdown]
+   [nrepl.cmdline :as nrepl])
   (:import
    [com.diozero.util Diozero])
-  (:require
-   [clojure.core.async :as async]
-   [clojure.tools.logging :as log]
-   [integrant.core :as ig]
-   [fairy.box.config :as config]
-   [fairy.box.env :refer [defaults]]
-   [signal.handler :as signal]
-   [jp.nijohando.event :as ev]
-   [fairy.box.web.handler]
-   [fairy.box.web.routes.api]
-   [fairy.box.web.routes.ui]
-   [fairy.box.db]
-   [fairy.box.bus]
-   [fairy.box.switchboard :as switchboard]
-   [fairy.box.audio.system2]
-   [fairy.box.hardware]
-   [fairy.box.settings]
-   [fairy.box.mqtt]
-   [fairy.box.tts])
   (:gen-class))
 
 ;; log uncaught exceptions in threads
@@ -32,55 +19,16 @@
                  :exception ex
                  :where (str "Uncaught exception on" (.getName thread))}))))
 
-(defonce system (atom nil))
-
-(defn stop-app! []
-  ((or (:stop defaults) (fn [])))
-  (some-> (deref system) (ig/halt!))
+(defn stop-jvm []
+  (hifi/stop @system/system)
+  (Diozero/shutdown)
   (shutdown-agents))
 
-(defn start-app [& [params]]
-  ((or (:start params) (:start defaults) (fn [])))
-  (->> (config/system-config (or (:opts params) (:opts defaults) {}))
-       (ig/expand)
-       (ig/init)
-       (reset! system))
-  (Diozero/initialiseShutdownHook)
-  (.addShutdownHook (Runtime/getRuntime) (Thread.  ^Runnable stop-app!)))
-
-(defn shutdown-loop [{:keys [value] :as event}]
-  (if (= :system/shutdown (:event value))
-    (do
-      (try
-        (log/info "system shutdown ready")
-        (stop-app!)
-        (Thread/sleep 5000)
-        (finally
-          (System/exit 0)))
-      nil)
-    :recur))
-
-(defn initiate-shutdown! [emitter bus]
-  (let [listener (async/chan)]
-    (prn "starting shutdown")
-    (ev/listen bus "/system" listener)
-    (async/go-loop []
-      (if (shutdown-loop (async/<! listener))
-        (recur)
-        (prn "shutdown-loop done")))
-    (switchboard/initiate-shutdown! emitter)))
-
-(defn -main [& _]
-  (start-app))
-
-(signal/with-handler :term
-  (try
-    (log/info "caught SIGTERM, quitting")
-    (initiate-shutdown!
-     (:emitter (:fairy.box.audio.system2/player @system))
-     (:fairy.box.bus/bus @system))
-    #_(stop-app!)
-    #_(log/info "all components shut down")
-    (catch Throwable t
-      (log/error t "Error during shutdown")
-      (println t))))
+(defn -main []
+  (let [system                  (system/start)
+        {:keys [enabled? args]} (get-in system [:donut.system/instances :env :hifi/repl])]
+    (shutdown/add-shutdown-hook! ::stop stop-jvm)
+    (when enabled?
+      (if args
+        (apply nrepl/-main args)
+        (log/info "No nREPL args provided under :hifi/repl, starting without nREPL.")))))
